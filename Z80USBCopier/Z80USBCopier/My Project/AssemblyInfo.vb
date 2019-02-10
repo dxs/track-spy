a5eb3598a742ef117afdd366e2eba31163866f96
Sven <glaglasven@live.com>
2019-02-10 16:24:14 +0100

    First update

---
+++ Z80USBCopier/Z80USBCopier/My Project/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly Is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+<Assembly: AssemblyTitle("Z80USBCopier")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Z80USBCopier")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76b95974-b872-43c0-9ea2-f7e979e13841")>
+
+' Version information for an assembly consists of the following four values
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values Or you can default the Build And Revision Numbers 
+' by using the '*' as shown below:
+' [assembly: AssemblyVersion("1.0.*")]
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Z80USBCopier/Z80USBCopier/ProjectInstaller.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration.Install
+
+Public Class ProjectInstaller
+
+    Public Sub New()
+        MyBase.New()
+
+        'Component Designer requires this call.
+        InitializeComponent()
+
+        'Add the initialization code after calling InitializeComponent
+
+    End Sub
+
+End Class
